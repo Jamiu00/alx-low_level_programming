3d61003b92ea79f36edee458de6cf7341c7a743c
LitCoder <adesinajamiu619@gmail.com>
2022-09-12 15:41:53 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ *
+ * This program prints a string to stdout
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
