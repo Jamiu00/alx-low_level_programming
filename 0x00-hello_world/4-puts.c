f9c4b51fb966d97f3c86205c905316a61f91120d
LitCoder <adesinajamiu619@gmail.com>
2022-09-12 19:03:06 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-/**
- * This program prints anstring on the stdout
- */
 #include <stdio.h>
 /**
- * main - print the string in the put function
- * return: 0
- */
+* main - prints "Programming is like building a multilingual puzzle
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
